23af257ad63dd554659252d13027d16b9ef92025
Boyaa <cbh0548@icloud.com>
2022-11-05 03:49:47 +0900

    Connect back and front

--- ZoomClone/src/server.js
+++ ZoomClone/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,6 +10,18 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req,res) => res.render("home"));
 app.get('/*', (req,res) => res.redirect("/"));
 
-console.log("helloworld");
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+	console.log("Connected to Browser 😎");
+	socket.on("close", () => console.log("Disconnected from the Browser 😯"));
+	socket.on("message", (message) => {
+		console.log(message.toString('utf8'));
+	});
+	socket.send("Boyaoom에 오신 것을 환영합니다 🐶");
+});
+
+server.listen(3000, handleListen);
