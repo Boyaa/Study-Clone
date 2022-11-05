4c37aa9272d2495fde1b31f136d1695ff85f1a9f
Boyaa <cbh0548@icloud.com>
2022-11-06 03:36:21 +0900

    install SocketIO and learn callback

--- ZoomClone/src/public/js/app.js
+++ ZoomClone/src/public/js/app.js
@@ -1,38 +1,18 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-function makeMessage(type, payload){
-    const msg = {type, payload}
-    return JSON.stringify(msg); // message string으로 바꿔서 return 해 줌 
-}
+const welcome = document.getElementById("welcome")
+const form = welcome.querySelector("form");
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server 😎")
-});
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server 😯")
-});
+function backendDone(msg) {
+    console.log(`The backend says: `, msg)
+};
 
 
-function handleSubmit(event) {
+function handleRoomSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
-    const li = document.createElement("li");
-    li.innerText = `You: ${input.value}`;
-    messageList.append(li);
+    const input = form.querySelector("input");
+    socket.emit("enter_room", { payload: input.value }, backendDone);
     input.value = "";
 }
 
-function handleNickSubmit(event) {
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
-}
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
