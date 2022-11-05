44df24fce7c36eca1778f9eb2e233486cb4c41de
Boyaa <cbh0548@icloud.com>
2022-11-06 00:51:09 +0900

    before apply socketIO

--- ZoomClone/src/public/js/app.js
+++ ZoomClone/src/public/js/app.js
@@ -1,17 +1,38 @@
-const socket = new WebSocket(`ws://${window.location.host}`)
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload){
+    const msg = {type, payload}
+    return JSON.stringify(msg); // message string으로 바꿔서 return 해 줌 
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server 😎")
-});
-
-socket.addEventListener("message", (message) => {
-    console.log("New Message: ", message.data)
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server 😯")
 });
 
-setTimeout(() => {
-    socket.send("Hello from the Browser!")
-}, 10000);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- ZoomClone/src/server.js
+++ ZoomClone/src/server.js
@@ -7,21 +7,31 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => res.render("home"));
-app.get('/*', (req,res) => res.redirect("/"));
+app.get("/", (_,res) => res.render("home"));
+app.get('/*', (_,res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+	sockets.push(socket);
+	socket["nickname"] = "익명이"
 	console.log("Connected to Browser 😎");
 	socket.on("close", () => console.log("Disconnected from the Browser 😯"));
-	socket.on("message", (message) => {
-		console.log(message.toString('utf8'));
+	socket.on("message", (msg) => {
+		const message = JSON.parse(msg);
+		switch(message.type) {
+			case "new_message":
+				sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));	
+			case "nickname":
+				socket["nickname"] = message.payload;
+		}
+		
 	});
-	socket.send("Boyaoom에 오신 것을 환영합니다 🐶");
 });
 
 server.listen(3000, handleListen);
