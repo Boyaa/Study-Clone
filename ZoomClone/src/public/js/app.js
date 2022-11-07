0c904261b69a96c056aebdcf2f4ba5daa0b4ad3e
Boyaa <cbh0548@icloud.com>
2022-11-07 17:52:32 +0900

    add comment

--- ZoomClone/src/public/js/app.js
+++ ZoomClone/src/public/js/app.js
@@ -45,7 +45,7 @@ function showRoom() {
 };
 
 
-function handleRoomSubmit(event){
+function handleRoomSubmit(event){ // 2 이 함수에서 방제와 닉네임이 변수로 만들어지고, socket에 enter_room이라는 event를 날린다. 여기에 방제와 닉네임, showroom 함수가 인자로 들어간다. 
     event.preventDefault();
     const input = title.querySelector("input");
     const entername = welcome.querySelector("input");
@@ -54,11 +54,11 @@ function handleRoomSubmit(event){
     input.value = "";
 }
 
-title.addEventListener("submit", handleRoomSubmit);
+title.addEventListener("submit", handleRoomSubmit); // 1 첫 번째로 방 제목 생성 버튼 눌러서 submit 되면 handleRoomSubmit 함수 호출됨 
+1
 
-
-socket.on("welcome", (user) => {
-    addMessage(`${user} joined!`);
+socket.on("welcome", (user) => {  // 4. welcome 이벤트가 socket에 들어오면 socket.nickname이 user라는 이름의 변수에 값으로 들어간다. 
+    addMessage(`${user} joined!`);  // 이 때 addMessage라는 함수가 발동되고 그 내용은 user joined
 });
 
 socket.on("bye", (user) => {
--- ZoomClone/src/server.js
+++ ZoomClone/src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
-import { SocketAddress } from "net";
 
 const app = express();
 
@@ -16,16 +15,16 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer)
 
-wsServer.on("connection", (socket) => {
+wsServer.on("connection", (socket) => { // 연결되어서 socket이 생성되면 호출됨 
 	socket["nickname"]="익명이";
 	socket.onAny((event) => {
 		console.log(`Socket Event: ${event}`)
 	})
-	socket.on("enter_room", (roomName, nickname, done) => {
-		socket["nickname"] = nickname;
-		socket.join(roomName);
-		done();
-		socket.to(roomName).emit("welcome", socket.nickname)
+	socket.on("enter_room", (roomName, nickname, done) => { // 3 enter_room이라는 event가 socket에 들어오면 방제, 닉네임, 함수가 인자로 들어온다.
+		socket["nickname"] = nickname						// socket에 nickname이라는 컬럼을 만들고 값으로 인자로 넘어온 닉네임 넣어준다 
+		socket.join(roomName);								// socket이 방제에 해당하는 방으로 들어간다.
+		done();												// showRoom() 함수 호출 
+		socket.to(roomName).emit("welcome", socket.nickname)	// 방제에 해당하는 곳에 socket.nickname이라는 인자를 넣어서 welcome event 만들어준다.
 	});
 	socket.on("disconnecting", () => {
 		socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
@@ -34,6 +33,7 @@ wsServer.on("connection", (socket) => {
 		socket.to(room).emit("got_new_message", `${socket.nickname}: ${msg}`);
 		done();
 	})
+	socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 	
 });
 
