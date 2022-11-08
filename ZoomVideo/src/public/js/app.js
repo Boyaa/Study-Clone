cd1523b38bb257c9f7e9ce30a9b8d5a159a1f648
Boyaa <cbh0548@icloud.com>
2022-11-08 17:52:11 +0900

    add Camera Option

--- ZoomVideo/src/public/js/app.js
+++ ZoomVideo/src/public/js/app.js
@@ -3,21 +3,33 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute")
 const cameraBtn = document.getElementById("camera")
+const camerasSelect = document.getElementById("cameras")
 
 let myStream;
 let muted = false; // 음소거가 해제된 상태로 시작 
 let cameraOff = false; //  카메라 켜져있는 상태로 시작
 
 async function getCameras() {
-
-}
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput") //video 출력하는 기기만 골라서 가져오기 
+        cameras.forEach(camera => { //카메라 출력하는 기기 리스트 하나씩 데려와서 
+            const option = document.createElement("option"); //본문에 option 태그 넣어주는 변수 생성
+            option.value = camera.deviceId; //그 옵션의 value로 deviceId 넣어주기 
+            option.innerText = camera.label; //그 옵션은 기기 label 값으로 보여지게 하기 
+            camerasSelect.appendChild(option); //본문의 option이라는 id를 가진 공간에 자식 태그들 추가
+        })
+    } catch (e) {
+        console.log(e);
+    }
+} // navigator.mediaDevices.enumerateDevices() promise 이기 때문에 try, catch, console.log(e) 해줘야 함
 
 async function getMedia() {
     try{
         myStream = await navigator.mediaDevices.getUserMedia({
             audio: true,
             video: true,
-        })
+        });
         myFace.srcObject = myStream;
         await getCameras();
     } catch (e) {
